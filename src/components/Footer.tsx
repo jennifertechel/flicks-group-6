d5f247b015edaa04b1f8b11616f504d2a3a040e5
HaNNa89 <petersson.h@hotmail.com>
2023-12-06 13:11:34 +0100

    change some styling comment out copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,20 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { PiCopyrightThin } from "react-icons/pi";
+import { Box, Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function Footer() {
 	return (
-		<Flex
-			py={30}
-			px={10}
-			bg={"black"}
-			color={"white"}
-			direction={{ base: "row", md: "column" }}
-		>
-			<Flex justifyContent={"space-between"} m={8}>
-				<Box fontSize={30}>
+		<Flex py={30} px={10} bg={"black"} color={"white"} direction={"column"}>
+			<Flex
+				direction={{ base: "column", sm: "row" }}
+				justifyContent={"space-between"}
+			>
+				<Box fontSize={30} mb={{ base: 4, sm: 0 }}>
 					<Link to="/">FLICKS</Link>
 				</Box>
-				<Flex direction={"column"}>
+
+				<Flex direction={"column"} mb={4}>
 					<Link to="categories">Categories</Link>
-					<Link to="/">Trendings</Link>
+					<Link to="trendings">Trendings</Link>
 					<Link to="favorites">Favorites</Link>
 				</Flex>
 
@@ -28,12 +25,13 @@ function Footer() {
 				</Flex>
 			</Flex>
 
-			<Flex justifyContent={"center"}>
+			{/*
+      <Flex justifyContent={"center"}>
 				<Text>Copyright 2023</Text>
 				<Box>
 					<PiCopyrightThin />
 				</Box>
-			</Flex>
+			</Flex>*/}
 		</Flex>
 	);
 }

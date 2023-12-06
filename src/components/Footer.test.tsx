7960901b48f8ccea1df02818a19fd132639561f5
HaNNa89 <petersson.h@hotmail.com>
2023-12-06 10:53:19 +0100

    create test for footer component - in progress

---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Footer from "./Footer";
+test("Logo should link correctly to start", () => {
+	render(
+		<MemoryRouter>
+			<Footer />
+		</MemoryRouter>
+	);
+	const startLink = screen.getByRole("link", { name: /flicks/i });
+	expect(startLink).toBeInTheDocument();
+});

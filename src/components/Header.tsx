ad88ebbfe6b4347b95ef97ee9f288a40a595fa3d
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-06 13:45:46 +0100

    add mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
-import { Flex, Heading, IconButton, Input } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { FiSearch } from "react-icons/fi";
+import {
+  Flex,
+  Heading,
+  Input,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerBody,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Header() {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleSearchClick = () => {
     setIsSearchOpen(!isSearchOpen);
   };
   return (
-    <Flex px={20} py={3} justify="space-between" align="center">
-      <Heading fontSize={72}>
+    <Flex
+      px={{ base: 5, md: 20 }}
+      py={3}
+      justify="space-between"
+      align="center"
+    >
+      <Heading fontSize={{ base: 68, md: 72 }}>
         <Link to="/">Flicks</Link>
       </Heading>
-      <Flex gap={10}>
+      <Flex gap={{ base: 2, md: 10 }}>
         <Flex position="relative" alignSelf="center">
           <IconButton
             icon={<FiSearch />}
@@ -25,18 +43,65 @@ function Header() {
             color="white"
             marginRight={isSearchOpen ? "5px" : 0}
             _hover={{ bg: "none" }}
+            data-testid="search-button"
           />
 
           {isSearchOpen && (
-            <Input placeholder="Search..." size="sm" marginTop="3px" />
+            <Input
+              placeholder="Search..."
+              size="sm"
+              marginTop="3px"
+              role="search"
+              data-testid="search-input"
+            />
           )}
         </Flex>
-        <Heading order={3}>
-          <Link to="categories">Categories</Link>
-        </Heading>
-        <Heading order={3}>
-          <Link to="favorites">Favorites</Link>
-        </Heading>
+        {/** Desktop */}
+        <Flex gap={10} display={{ base: "none", md: "flex" }}>
+          <Heading order={3}>
+            <Link to="categories">Categories</Link>
+          </Heading>
+          <Heading order={3}>
+            <Link to="favorites">Favorites</Link>
+          </Heading>
+        </Flex>
+        {/** Mobile */}
+        <IconButton
+          icon={<RxHamburgerMenu />}
+          aria-label="menu"
+          fontSize={30}
+          bg="none"
+          color="white"
+          _hover={{ bg: "none" }}
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        />
+        <Drawer isOpen={isOpen} size="full" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerBody bg="#202020">
+              <DrawerCloseButton
+                paddingTop={12}
+                paddingRight={8}
+                fontSize={20}
+              />
+              <Flex
+                gap={10}
+                flexDir="column"
+                paddingTop={40}
+                justifyItems="center"
+                alignItems="center"
+              >
+                <Heading order={3}>
+                  <Link to="categories">Categories</Link>
+                </Heading>
+                <Heading order={3}>
+                  <Link to="favorites">Favorites</Link>
+                </Heading>
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
       </Flex>
     </Flex>
   );

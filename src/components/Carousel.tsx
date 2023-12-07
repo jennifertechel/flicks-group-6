f061085f847572ee8fff83916e28651eacae5031
HaNNa89 <petersson.h@hotmail.com>
2023-12-07 21:48:03 +0100

    add carousels for each genre

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,69 +1,69 @@
 import { Flex } from "@chakra-ui/react";
-import Thumbnail from "./Thumbnail";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Thumbnail from "./Thumbnail";
 
 interface Movie {
-  title: string;
-  year: number;
-  rating: string;
-  thumbnail: string;
-  genre: string;
+	title: string;
+	year: number;
+	rating: string;
+	thumbnail: string;
+	genre: string;
 }
 
 interface CarouselProps {
-  movies: Movie[];
+	movies: Movie[];
 }
 
 function Carousel({ movies }: CarouselProps) {
-  return (
-    <Flex w="100%" justifyContent="center" alignItems="center">
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        breakpoints={{
-          480: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          992: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1280: {
-            slidesPerView: 5,
-            spaceBetween: 40,
-          },
-          1536: {
-            slidesPerView: 6,
-            spaceBetween: 50,
-          },
-        }}
-      >
-        {movies.map((movie: Movie, index: number) => (
-          <SwiperSlide key={index}>
-            <Thumbnail
-              genre={movie.genre}
-              title={movie.title}
-              image={movie.thumbnail}
-              rating={movie.rating}
-              year={movie.year}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Flex>
-  );
+	return (
+		<Flex w="100%" justifyContent="center" alignItems="center">
+			<Swiper
+				modules={[Navigation]}
+				spaceBetween={20}
+				slidesPerView={1}
+				navigation
+				breakpoints={{
+					480: {
+						slidesPerView: 2,
+						spaceBetween: 20,
+					},
+					768: {
+						slidesPerView: 3,
+						spaceBetween: 30,
+					},
+					992: {
+						slidesPerView: 3,
+						spaceBetween: 40,
+					},
+					1280: {
+						slidesPerView: 5,
+						spaceBetween: 40,
+					},
+					1536: {
+						slidesPerView: 6,
+						spaceBetween: 50,
+					},
+				}}
+			>
+				{movies.map((movie: Movie, index: number) => (
+					<SwiperSlide key={index}>
+						<Thumbnail
+							genre={movie.genre}
+							title={movie.title}
+							image={movie.thumbnail}
+							rating={movie.rating}
+							year={movie.year}
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</Flex>
+	);
 }
 
 export default Carousel;
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,21 +1,24 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
-import React from "react";
 import data from "../../data/movies.json";
 
+const genres = Array.from(
+	new Set(data.flatMap((movie) => movie.genre.split(", ")))
+);
+
 function Categories() {
-  return (
-    <Flex flexDir="column" px={35}>
-      <Heading>Categories</Heading>
-      <Flex flexDir="column">
-        {data.map((movie, index) => (
-          <Box key={index}>
-            <Text>{movie.title}</Text>
-            <Text>{movie.genre}</Text>
-          </Box>
-        ))}
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex flexDir="column" px={35}>
+			<Heading>Categories</Heading>
+			<Flex flexDir="column">
+				{data.map((movie, index) => (
+					<Box key={index}>
+						<Text>{movie.title}</Text>
+						<Text>{movie.genre}</Text>
+					</Box>
+				))}
+			</Flex>
+		</Flex>
+	);
 }
 
 export default Categories;

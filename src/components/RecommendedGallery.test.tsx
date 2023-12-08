b45df033918e328fdca6d5341f024b1496425221
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-08 13:41:13 +0100

    fix casing bug on filename

--- src/components/RecommendedGallery.test.tsx
+++ src/components/RecommendedGallery.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import RecommendedGallery from './RecommendedGallery';
+import { render, screen } from "@testing-library/react";
+import RecommendedGallery from "./RecommendedGallery";
 
-describe('RecommendedGallery component', () => {
-  test('renders between 5 and 10 recommended movies', () => {
+describe("RecommendedGallery component", () => {
+  test("renders between 5 and 10 recommended movies", () => {
     render(<RecommendedGallery />);
-    
-    const movieThumbnails = screen.getAllByRole('img');
+
+    const movieThumbnails = screen.getAllByRole("img");
     const numberOfMovies = movieThumbnails.length;
 
     expect(numberOfMovies).toBeGreaterThanOrEqual(5);

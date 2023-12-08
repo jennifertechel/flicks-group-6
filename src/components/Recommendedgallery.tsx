b960ea4d93b4ebcdb2c1ceea8c0895ef92f587b6
lauax <lucas12alfredsson@gmail.com>
2023-12-08 13:57:14 +0100

    RecommendedGallery nu en carousel

--- src/components/Recommendedgallery.tsx
+++ src/components/Recommendedgallery.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import data from "../../data/movies.json";
 import Thumbnail from "./Thumbnail";
 
@@ -10,17 +12,54 @@ interface Movie {
   genre: string;
 }
 
+
 function RecommendedGallery() {
-  const RecommendedMovies = data.slice(0, 5);
+  const moviesPerPage = 5;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const trendingMovies = data.slice(0, 5);
+  
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === trendingMovies.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? trendingMovies.length - 1 : prevIndex - 1
+    );
+  };
+
+  const startSliceIndex = currentIndex * moviesPerPage;
+  const endSliceIndex = startSliceIndex + moviesPerPage;
+
+  const RecommendedMovies = trendingMovies.slice(startSliceIndex, endSliceIndex);
+
 
   return (
-    <Flex justifyContent="space-evenly" flexWrap="wrap" gap="20px">
+
+    <Flex justifyContent="space-evenly">
+      <IconButton
+        aria-label="Previous"
+        icon={<FaChevronLeft />}
+        onClick={handlePrev}
+        left="0"
+        top="50%"
+        zIndex="1"
+        position="absolute"
+        isDisabled={trendingMovies.length <= moviesPerPage || currentIndex === 0}
+      />
       {RecommendedMovies.map((movie: Movie) => (
         <Box
-          key={movie.title}
-          boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2)"
-          borderRadius="md"
-        >
+        key={movie.title}
+        display="inline-block"
+        borderRadius="md"
+        minWidth="19vw"
+        marginRight="20px"
+        flexShrink={0}
+      >
           <Thumbnail
             genre={movie.genre}
             title={movie.title}
@@ -29,8 +68,23 @@ function RecommendedGallery() {
             year={movie.year}
           />
         </Box>
+        
       ))}
+      <IconButton
+        aria-label="Next"
+        icon={<FaChevronRight />}
+        onClick={handleNext}
+        position="absolute"
+        right="0"
+        top="50%"
+        zIndex="1"
+        isDisabled={
+        trendingMovies.length <= moviesPerPage ||
+        currentIndex === Math.ceil(trendingMovies.length / moviesPerPage) - 1
+        }
+      />
     </Flex>
+    
   );
 }
 

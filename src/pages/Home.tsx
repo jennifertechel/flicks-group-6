c8c1e09003554a86d706d6df1c1d2a773df047c1
Tara skoglund <skoglundtara@gmail.com>
2023-12-11 15:33:24 +0100

    just to se

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import { Flex, Heading } from "@chakra-ui/react";
+import Thumbnail from "../components/Thumbnail";
 
 function Home() {
   return (
     <Flex>
       <Heading>Home</Heading>
+      <Thumbnail
+        image={
+          "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg"
+        }
+        rating={"R"}
+        year={1987}
+        title={"The Shawshank Redemption"}
+        genre={"Drama"}
+      />
     </Flex>
   );
 }

c75db0cbf2a32b03f7a2d590f47714d0b74b13bc
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-12 11:25:48 +0100

    add carousels to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { Flex, Heading } from "@chakra-ui/react";
-import RecommendedGallery from "../components/RecommendedGallery";
+import RecommendedGallery from "../components/Recommendedgallery";
+import TrendingCarousel from "../components/TrendingCarousel";
 
 function Home() {
   return (
-    <Flex>
-      <Heading>Home</Heading>
-      <RecommendedGallery></RecommendedGallery>
+    <Flex px={35} flexDirection="column" gap={10}>
+      <Heading>Trending right now</Heading>
+      <TrendingCarousel />
+      <Heading>Recommended for you</Heading>
+      <RecommendedGallery />
     </Flex>
   );
 }

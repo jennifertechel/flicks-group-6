db80cb9a1a5cb28d75faa67c199fc657931a3948
HaNNa89 <petersson.h@hotmail.com>
2023-12-13 23:04:14 +0100

    add bg-image, heading and a link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Hero() {
-	return <Flex>Hero</Flex>;
+	return (
+		<Flex
+			w="full"
+			h="70vh"
+			flexDir="column"
+			justifyContent="center"
+			bgImage={`url(https://image.jimcdn.com/app/cms/image/transf/dimension=2080x10000:format=jpg/path/s2217cd0bb1220415/image/i1f8a44dd5ee0a217/version/1696589143/a-roman-gladiator-wearing-a-detailed-bronze-helmet-with-intricate-engravings.jpg)`}
+			bgSize="cover"
+			bgPos="center"
+		>
+			<Box>
+				<Heading>Welcome! Flicks</Heading>
+				<Link to="/categories">View more</Link>
+			</Box>
+		</Flex>
+	);
 }
 export default Hero;

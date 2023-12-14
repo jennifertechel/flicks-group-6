c2caafb2ba54f4b4c49b277975b7cac728eda132
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-14 12:45:13 +0100

    create test carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,15 @@ import RecommendedGallery from "../components/Recommendedgallery";
 import TrendingCarousel from "../components/TrendingCarousel";
 
 function Home() {
-	return (
-		<Flex px={35} flexDirection="column" gap={10}>
-			<Hero />
-			<Heading>Trending right now</Heading>
-			<TrendingCarousel />
-			<Heading>Recommended for you</Heading>
-			<RecommendedGallery />
-		</Flex>
-	);
+  return (
+    <Flex px={35} flexDirection="column" gap={10}>
+      <Hero />
+      <Heading>Trending right now</Heading>
+      <TrendingCarousel />
+      <Heading>Recommended for you</Heading>
+      <RecommendedGallery />
+    </Flex>
+  );
 }
 
 export default Home;

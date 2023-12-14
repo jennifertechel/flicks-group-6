54b9736fc57e6df5e5f078ad3149cddadd1a0931
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-14 12:45:13 +0100

    create test carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Flex, Heading } from "@chakra-ui/react";
+import Carousel2 from "../components/Carousel2";
 
 function Home() {
   return (
-    <Flex>
+    <Flex flexDir="column">
       <Heading>Home</Heading>
+      <Carousel2 />
     </Flex>
   );
 }

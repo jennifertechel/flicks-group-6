c7e624592b3ff31ac218204a6dbb9e77f2188252
HaNNa89 <petersson.h@hotmail.com>
2023-12-15 22:37:38 +0100

    add three tests n put them in one test block instead + add a data-testid in carousel component

--- src/pages/Categories.test.tsx
+++ src/pages/Categories.test.tsx
@@ -1,26 +1,57 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router";
 import Carousel from "../components/Carousel";
 import { LikeContextProvider } from "../hooks/useLikeContext";
+import Categories from "./Categories";
 
 const movieData = [
 	{
 		thumbnail: "movie_image.jpg",
 		rating: "R",
 		year: 2022,
-		title: "Test Movie",
+		title: "The Dark Knight",
 		genre: "Action",
 	},
 ];
-test("should display genre 'Actions' correctly", () => {
-	render(
-		<MemoryRouter>
-			<LikeContextProvider>
-				<Carousel movies={movieData} />
-			</LikeContextProvider>
-		</MemoryRouter>
-	);
-	const genre = screen.getByText(/Action/i);
-	expect(genre).toBeInTheDocument();
+
+describe("Categories component", () => {
+	test("should render carousel component correct into categories page", () => {
+		render(
+			<MemoryRouter>
+				<LikeContextProvider>
+					<Categories />
+				</LikeContextProvider>
+			</MemoryRouter>
+		);
+		const carousel = screen.findByTestId("carousel-container");
+		expect(carousel).toBeTruthy();
+	});
+
+	test("should display genre 'Actions' correctly", () => {
+		render(
+			<MemoryRouter>
+				<LikeContextProvider>
+					<Carousel movies={movieData} />
+				</LikeContextProvider>
+			</MemoryRouter>
+		);
+		const genre = screen.getByText(/Action/i);
+		expect(genre).toBeInTheDocument();
+	});
+
+	test("should navigate to details page when user clicked on a movie", async () => {
+		render(
+			<MemoryRouter>
+				<LikeContextProvider>
+					<Carousel movies={movieData} />
+				</LikeContextProvider>
+			</MemoryRouter>
+		);
+		const movieCard = screen.getByText(/The Dark Knight/i);
+		const user = userEvent.setup();
+		expect(movieCard).toBeInTheDocument();
+		await user.click(movieCard);
+	});
 });

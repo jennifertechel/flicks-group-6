660f7319fb4ceda7820cbcc9c7b05f6e2b2ca955
HaNNa89 <petersson.h@hotmail.com>
2023-12-15 11:55:18 +0100

    render all and genre and used the new carousel

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,21 +1,48 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
-import React from "react";
 import data from "../../data/movies.json";
+import Carousel from "../components/Carousel";
+import Thumbnail from "../components/Thumbnail";
+
+const genres = Array.from(
+	new Set(data.flatMap((movie) => movie.genre.split(", ")))
+);
 
 function Categories() {
-  return (
-    <Flex flexDir="column" px={35}>
-      <Heading>Categories</Heading>
-      <Flex flexDir="column">
-        {data.map((movie, index) => (
-          <Box key={index}>
-            <Text>{movie.title}</Text>
-            <Text>{movie.genre}</Text>
-          </Box>
-        ))}
-      </Flex>
-    </Flex>
-  );
-}
+	return (
+		<Flex flexDir="column">
+			<Heading>Categories</Heading>
 
+			<Flex flexDir="column">
+				{genres.map((genre) => {
+					const moviesInGenre = data.filter((movie) =>
+						movie.genre.includes(genre)
+					);
+					if (moviesInGenre.length > 4) {
+						return (
+							<Box key={genre}>
+								<Text>{genre}</Text>
+								<Carousel genre={genre} movies={moviesInGenre} />
+							</Box>
+						);
+					}
+					return (
+						<Flex flexDir="column" key={genre}>
+							<Text>{genre}</Text>
+							{moviesInGenre.map((movie) => (
+								<Thumbnail
+									key={movie.title}
+									genre={movie.genre}
+									title={movie.title}
+									image={movie.thumbnail}
+									rating={movie.rating}
+									year={movie.year}
+								/>
+							))}
+						</Flex>
+					);
+				})}
+			</Flex>
+		</Flex>
+	);
+}
 export default Categories;

009f5ee4f74e29fd81b50c742122c423e28e8b39
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-17 20:42:18 +0100

    remove type and add padding

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -4,50 +4,50 @@ import Carousel from "../components/Carousel";
 import Thumbnail from "../components/Thumbnail";
 
 const genres = Array.from(
-	new Set(data.flatMap((movie) => movie.genre.split(", ")))
+  new Set(data.flatMap((movie) => movie.genre.split(", ")))
 );
 
 function Categories() {
-	return (
-		<Flex flexDir="column">
-			<Heading mb={4}>Categories</Heading>
-			<Flex flexDir="column">
-				{genres.map((genre) => {
-					const moviesInGenre = data.filter((movie) =>
-						movie.genre.includes(genre)
-					);
-					if (moviesInGenre.length > 4) {
-						return (
-							<Box key={genre}>
-								<Text mt={4} mb={4}>
-									{genre}
-								</Text>
-								<Carousel genre={genre} movies={moviesInGenre} />
-							</Box>
-						);
-					}
-					return (
-						<Flex flexDir="column" key={genre}>
-							<Text mt={4} mb={4}>
-								{genre}
-							</Text>
-							<Flex flexDir="row" gap="20">
-								{moviesInGenre.map((movie) => (
-									<Thumbnail
-										key={movie.title}
-										genre={movie.genre}
-										title={movie.title}
-										image={movie.thumbnail}
-										rating={movie.rating}
-										year={movie.year}
-									/>
-								))}
-							</Flex>
-						</Flex>
-					);
-				})}
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex flexDir="column" px={35}>
+      <Heading mb={4}>Categories</Heading>
+      <Flex flexDir="column">
+        {genres.map((genre) => {
+          const moviesInGenre = data.filter((movie) =>
+            movie.genre.includes(genre)
+          );
+          if (moviesInGenre.length > 4) {
+            return (
+              <Box key={genre}>
+                <Text mt={4} mb={4}>
+                  {genre}
+                </Text>
+                <Carousel movies={moviesInGenre} />
+              </Box>
+            );
+          }
+          return (
+            <Flex flexDir="column" key={genre}>
+              <Text mt={4} mb={4}>
+                {genre}
+              </Text>
+              <Flex flexDir="row" gap="20">
+                {moviesInGenre.map((movie) => (
+                  <Thumbnail
+                    key={movie.title}
+                    genre={movie.genre}
+                    title={movie.title}
+                    image={movie.thumbnail}
+                    rating={movie.rating}
+                    year={movie.year}
+                  />
+                ))}
+              </Flex>
+            </Flex>
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
 }
 export default Categories;
